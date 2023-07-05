4c56b54ec72b54439e160fb76825d558e8c98f0c
Ivan Robles <irae45@gmail.com>
2023-07-05 17:09:02 -0600

    Add routes for Users and Projects.

--- routes/users.router.js
+++ routes/users.router.js
@@ -6,9 +6,37 @@ router.get('/', (req, res) => {
   res.send('Users')
 });
 
+router.post('/', (req, res) => {
+  const { body } = req;
+  res.status(201).json({
+    message: 'created',
+    data: body
+  });
+});
+
 router.get('/:userId', (req, res) => {
   const userId = req.params;
   res.send(`User ${userId.userId}`)
+});
+
+router.patch('/:userId', (req, res) => {
+  const { body } = req;
+  const { userId } = req.params;
+
+  res.json({
+    message: 'updated',
+    userId,
+    data: body
+  });
+});
+
+router.delete('/:userId', (req, res) => {
+  const { userId } = req.params;
+
+  res.json({
+    message: 'deleted',
+    userId,
+  });
 });
 
 router.get('/:userId/jobs', (req, res) => {
@@ -47,6 +75,34 @@ router.delete('/:userId/jobs/:jobId', (req, res) => {
 router.get('/:userId/projects', (req, res) => {
   const userId = req.params;
   res.send(`User ${userId.userId} Projects`)
+});
+
+router.post('/:userId/projects', (req, res) => {
+  const { body } = req;
+  res.status(201).json({
+    message: 'created',
+    data: body
+  });
+});
+
+router.patch('/:userId/projects/:projectId', (req, res) => {
+  const { body } = req;
+  const { projectId } = req.params;
+
+  res.json({
+    message: 'updated',
+    projectId,
+    data: body
+  });
+});
+
+router.delete('/:userId/projects/:projectId', (req, res) => {
+  const { projectId } = req.params;
+
+  res.json({
+    message: 'deleted',
+    projectId,
+  });
 });
 
 module.exports = router;
