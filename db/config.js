23a1eea4b8c0bf09c75e7d95c3512eb291e0fca3
Ivan Robles <irae45@gmail.com>
2023-08-10 18:30:04 -0600

    add configs to handling the database in production.

--- db/config.js
+++ db/config.js
@@ -2,15 +2,20 @@ const config = require('../config/config');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const getURI = (dialect) => `${dialect}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 module.exports = {
   development: {
-    url: URI,
+    url: getURI('postgres'),
     dialect: 'postgres',
   },
   production: {
-    url: URI,
-    dialect: 'postgres',
+    url: getURI('mysql'),
+    dialect: 'mysql',
+    dialectOptions: {
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
   }
 };
