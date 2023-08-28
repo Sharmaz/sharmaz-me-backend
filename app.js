10e40d7f96d50c9698276a8002943c4a356ad488
Ivan Robles <irae45@gmail.com>
2023-08-28 17:49:39 -0600

    refactor express app for test e2e environment.

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { engine } = require('express-handlebars');
+const config = require('./config/config');
+const { routerApi, routerViews } = require('./routes');
+const { logErrors, boomErrorHandler } = require('./middlewares/error.handler');
+
+const createApp = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+
+  const whitelist = config.allowedList;
+
+  const options = {
+    origin: (origin, callback) => {
+      if (whitelist.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not Allowed'));
+      }
+    }
+  }
+
+  app.engine('.hbs', engine({extname: 'hbs'}));
+  app.set('view engine', '.hbs');
+  app.set('views', './views');
+
+  const publicPath = path.join(__dirname, './views');
+
+  app.use(cors(options));
+
+  // eslint-disable-next-line global-require
+  require('./utils/auth');
+
+  app.use('/', express.static(publicPath));
+
+  routerApi(app);
+  routerViews(app);
+
+  app.use(logErrors);
+  app.use(boomErrorHandler);
+
+  return app;
+}
+
+module.exports = createApp;
--- tests/e2e/app.e2e.test.js
+++ tests/e2e/app.e2e.test.js
@@ -1,30 +1,26 @@
 const request = require('supertest');
-const express = require('express');
 const { expect, beforeEach, afterEach } = require('@jest/globals');
+
+const createApp = require('../../app');
 
 let app;
 let api;
 let server;
 
 beforeEach(() => {
-  app = express();
-  app.get('/hello', (req, res) => {
-    res.status(200).json({ message: 'hello!!'});
-  });
+  app = createApp();
 
   server = app.listen(9000);
   api = request(app);
-})
+});
 
 afterEach(() => {
   server.close();
-})
+});
 
 describe('app test', () => {
-  test('get /hello endpoint', async() => {
-    const response = await api.get('/hello');
+  test('get /', async() => {
+    const response = await api.get('/');
     expect(response).toBeTruthy();
-    expect(response.statusCode).toBe(200);
-    expect(response.body.message).toBe('hello!!');
   });
 });
