32f71a4ffe6e76e412bd978c274e42b3d36b391d
Ivan Robles <irae45@gmail.com>
2023-08-28 18:36:18 -0600

    add end to end test enviroment database config.

--- config/config.js
+++ config/config.js
@@ -1,7 +1,19 @@
-require('dotenv').config();
+const env = process.env.NODE_ENV || 'dev';
+const envs = {
+  'dev': '.env',
+  'e2e': '.env.e2e'
+}
+
+const options = {};
+
+if (envs[env]) {
+  options.path = envs[env];
+}
+
+require('dotenv').config(options);
 
 const config = {
-  env: process.env.NODE_ENV || 'dev',
+  env,
   isProd: process.env.NODE_ENV === 'production',
   port: process.env.PORT || 3000,
   dbUser: process.env.DB_USER,
